73fa09c83361f840c47ab51f21008d2e698c703e
Matthieu Masta Denis <matthieu.denis@abitus.net>
2019-09-15 23:38:02 +0200

    Add SignIn template

---
+++ src/php/templates/SignIn.php
@@ -0,0 +1,10 @@
+<h1>Connexion</h1>
+
+<form method="post" action="">
+	<label for="login">Identifiant / email</label>
+	<input type="text" name="login" id="login" required>
+	<br>
+	<label for="password">Mot de Passe</label>
+	<input type="password" name="password" id="password" required>
+	<input type="submit" value="Ok">
+</form>
