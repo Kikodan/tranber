7f7c5ee4edf509bc5b140e92c6fb2a02af6212a8
Matthieu Masta Denis <matthieu.denis@abitus.net>
2019-09-15 23:43:09 +0200

    Update Navbar template: add sign-in and sign-out links

--- src/php/templates/Navbar.php
+++ src/php/templates/Navbar.php
@@ -1,8 +1,12 @@
 <?php
 
-$title       = $title   ?? 'Tranber';
-$siteUrl     = $siteUrl ?? '';
-$signUpUrl   = $siteUrl.'sign-up';
+$clientIsLogged = $clientIsLogged ?? false;
+$title          = $title          ?? 'Tranber';
+$siteUrl        = $siteUrl        ?? '';
+
+$signInUrl      = $siteUrl.'sign-in';
+$signUpUrl      = $siteUrl.'sign-up';
+$signOutUrl     = $siteUrl.'sign-out';
 
 ?><nav class="blk_Navbar">
 	<div class="elm_Navbar_Title">
@@ -12,9 +16,18 @@ $signUpUrl   = $siteUrl.'sign-up';
 	</div>
 	<div class="elm_Navbar_Menu">
 		<ul class="blk_Menu blk_Menu-navbar">
+<?php if ($clientIsLogged): ?>
+			<li class="elm_Menu_Item elm_Menu_Item-navbar">
+				<a href="<?= $signOutUrl ?>" class="atm_Link atm_Link-menu atm_Link-navbar atm_Link-navbarMenu">Sign out</a>
+			</li>
+<?php else: ?>
+			<li class="elm_Menu_Item elm_Menu_Item-navbar">
+				<a href="<?= $signInUrl ?>" class="atm_Link atm_Link-menu atm_Link-navbar atm_Link-navbarMenu">Sign in</a>
+			</li>
 			<li class="elm_Menu_Item elm_Menu_Item-navbar">
 				<a href="<?= $signUpUrl ?>" class="atm_Link atm_Link-menu atm_Link-navbar atm_Link-navbarMenu">Sign up</a>
 			</li>
+<?php endif ?>
 		</ul>
 	</div>
 </nav>
